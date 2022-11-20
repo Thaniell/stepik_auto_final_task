d2196a11441c6b550ef436fe018e24ee9f93c5fd
Thaniell <thaniellfox@gmail.com>
2022-11-20 17:41:18 +0300

    change test_product_page to parametrize links

--- test_product_page.py
+++ test_product_page.py
@@ -1,15 +1,14 @@
 from .pages.product_page import ProductPage
-import time
-from .pages.locators import ProductPageLocators
+import pytest
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('link', ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
+def test_guest_can_add_product_to_basket(browser, link):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{link}"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
-    #time.sleep(60)
     page.should_be_product_name()
     page.should_be_product_price()
 
